9f8827bc6ebf04b0b9b3c0db5b05a4bc23a33b84
André Schließer <andy.schliesser@gmail.com>
2021-02-17 15:06:38 +0100

    Try to add translations

--- src/main.lua
+++ src/main.lua
@@ -21,4 +21,35 @@ function Survival:loadMap()
     source(modDirectory .. "src/shop.lua")
 end
 
+function Survival:init()
+    print('Survival: init')
+    Survival.addModTranslations(g_i18n)
+end
+
+function Survival:delete()
+    print('Survival: delete')
+    Survival.removeModTranslations(g_i18n)
+end
+
+---Copy our translations to global space.
+function Survival.addModTranslations(i18n)
+    print('Survival: addModTranslations')
+    -- We can copy all our translations to the global table because we prefix everything with survival_
+    -- The mod-based l10n lookup only really works for vehicles, not UI and script mods.
+    local global = getfenv(0).g_i18n.texts
+
+    for key, text in pairs(i18n.texts) do
+        global[key] = text
+    end
+end
+
+---Remove mod translations to prevent duplicated next time it gets loaded
+function Survival.removeModTranslations(i18n)
+    print('Survival: removeModTranslations')
+    local global = getfenv(0).g_i18n.texts
+    for key, text in pairs(i18n.texts) do
+        global[key] = nil
+    end
+end
+
 addModEventListener(Survival)
--- src/shop.lua
+++ src/shop.lua
@@ -7,6 +7,6 @@
 -- override lease button callback
 function ShopConfigScreen:onYesNoLease(yes)
     g_gui:showInfoDialog({
-        text = "Leasing is disabled!"
+        text = g_i18n:getText("survival_shopLeaseDisabled")
     })
 end
