72e6fa8c5e7fbef313fc8a7723c6164640255281
André Schließer <andy.schliesser@gmail.com>
2021-02-17 15:04:26 +0100

    Override shop lease button callback

--- src/main.lua
+++ src/main.lua
@@ -4,14 +4,21 @@
 -- Copyright (c) Schliesser, 2021
 ----------------------------------------------------------------------------------------------------
 
-print('Survival: Try not to die!')
-
 -- Do not reset this variable, we can't re-set it again
 local modDirectory = g_currentModDirectory
 
--- Load files
-source(modDirectory .. "src/noLoan.lua")
-source(modDirectory .. "src/noSleep.lua")
-source(modDirectory .. "src/missions.lua")
-source(modDirectory .. "src/helper.lua")
-source(modDirectory .. "src/shop.lua")
+Survival = {}
+Survival.name = "Survival"
+
+function Survival:loadMap()
+    print('Survival: Try not to die!')
+
+    -- Load files
+    source(modDirectory .. "src/noLoan.lua")
+    source(modDirectory .. "src/noSleep.lua")
+    source(modDirectory .. "src/missions.lua")
+    source(modDirectory .. "src/helper.lua")
+    source(modDirectory .. "src/shop.lua")
+end
+
+addModEventListener(Survival)
