2d7ad0d417dea8b9ce773b17b954693e556dfd4c
André Schließer <andy.schliesser@gmail.com>
2021-02-17 12:18:05 +0100

    Disable helper for vehicles

--- src/main.lua
+++ src/main.lua
@@ -4,8 +4,13 @@
 -- Copyright (c) Schliesser, 2021
 ----------------------------------------------------------------------------------------------------
 
-print('Init survial mod')
+print('Survival: Try not to die!')
 
-source("src/noLoan.lua")
-source("src/noSleep.lua")
-source("src/missions.lua")
+-- Do not reset this variable, we can't re-set it again
+local modDirectory = g_currentModDirectory
+
+-- Load files
+source(modDirectory .. "src/noLoan.lua")
+source(modDirectory .. "src/noSleep.lua")
+source(modDirectory .. "src/missions.lua")
+source(modDirectory .. "src/helper.lua")
